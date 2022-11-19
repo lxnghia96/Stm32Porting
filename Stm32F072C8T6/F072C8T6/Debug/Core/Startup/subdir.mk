ad68c9d755f75c1156149268e65662e7ec86c5f1
unknown <lxnghia96@gmail.com>
2022-11-19 14:56:21 +0700

    add prj

--- Stm32F072C8T6/F072C8T6/Debug/Core/Startup/subdir.mk
+++ Stm32F072C8T6/F072C8T6/Debug/Core/Startup/subdir.mk
@@ -3,17 +3,17 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-S_SRCS += \
-../Core/Startup/startup_stm32f072c8tx.s 
+S_UPPER_SRCS += \
+../Core/Startup/startup_stm32f072c8tx.S 
 
 OBJS += \
 ./Core/Startup/startup_stm32f072c8tx.o 
 
-S_DEPS += \
+S_UPPER_DEPS += \
 ./Core/Startup/startup_stm32f072c8tx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Startup/startup_stm32f072c8tx.o: ../Core/Startup/startup_stm32f072c8tx.s
+Core/Startup/startup_stm32f072c8tx.o: ../Core/Startup/startup_stm32f072c8tx.S
 	arm-none-eabi-gcc -mcpu=cortex-m0 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Core/Startup/startup_stm32f072c8tx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 
